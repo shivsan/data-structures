9e80cfc983152aac9d3a12425b14fcbfbaddf52c
Siva Kumar <shivku.goku@gmail.com>
2021-04-04 20:33:00 +0530

    Added solution to n nearest neighbours in a graph

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -2,11 +2,7 @@ package graph;
 
 import java.util.*;
 
-public class NthNearestNeighbours {
-
-}
-
-class Graph {
+public class Graph {
     private Node root;
 
     Graph(Node root) {
@@ -62,6 +58,39 @@ class Graph {
 
         return null;
     }
+
+    public List<Node> findNthNeighbours(int depth) {
+        List<Node> depthNNeighbours = new ArrayList<>();
+
+        if (root == null)
+            return null;
+
+        Node currentNode = root;
+        Set<Node> visited = new HashSet<>();
+        Deque<NodeWithDepth<Node, Integer>> stack = new LinkedList<>();
+        stack.add(new NodeWithDepth<>(currentNode, 0));
+
+        while (!stack.isEmpty()) {
+            NodeWithDepth<Node, Integer> nodeWithDepth = stack.pop();
+            Node node = nodeWithDepth.node;
+            int currentDepth = nodeWithDepth.depth;
+
+            if (currentDepth > depth)
+                continue;
+
+            visited.add(node);
+
+            if (currentDepth == depth) {
+                depthNNeighbours.add(node);
+            } else
+                node.neighbours.forEach(neighbour -> {
+                    if (!visited.contains(neighbour))
+                        stack.add(new NodeWithDepth<>(neighbour, currentDepth + 1));
+                });
+        }
+
+        return depthNNeighbours;
+    }
 }
 
 class Node {
@@ -71,5 +100,15 @@ class Node {
     public Node(int i) {
         value = i;
         neighbours = new ArrayList<>();
+    }
+}
+
+class NodeWithDepth<K, V> {
+    K node;
+    V depth;
+
+    public NodeWithDepth(K node, V depth) {
+        this.node = node;
+        this.depth = depth;
     }
 }
--- src/test/java/graph/GraphTest.java
+++ src/test/java/graph/GraphTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
@@ -26,6 +28,14 @@ public class GraphTest {
         Node searchNode = graph.searchNodeDfs(5);
 
         assertEquals(5, searchNode.value);
+    }
+    @Test
+    public void getNthNearestNeighbours() {
+        Graph graph = createGraph();
+
+        List<Node> nthNeighbours = graph.findNthNeighbours(3);
+
+        assertEquals(3, nthNeighbours.size());
     }
 
     private Graph createGraph() {
@@ -49,12 +59,24 @@ public class GraphTest {
         rootNodeNeighbour11.neighbours.add(rootNodeNeighbour21);
 
         Node rootNodeNeighbour22 = new Node(6);
-        rootNodeNeighbour22.neighbours.add(rootNodeNeighbour21);
-        rootNodeNeighbour21.neighbours.add(rootNodeNeighbour22);
+        rootNodeNeighbour22.neighbours.add(rootNodeNeighbour12);
+        rootNodeNeighbour12.neighbours.add(rootNodeNeighbour22);
 
         Node rootNodeNeighbour23 = new Node(7);
-        rootNodeNeighbour23.neighbours.add(rootNodeNeighbour22);
-        rootNodeNeighbour22.neighbours.add(rootNodeNeighbour23);
+        rootNodeNeighbour23.neighbours.add(rootNodeNeighbour12);
+        rootNodeNeighbour12.neighbours.add(rootNodeNeighbour23);
+
+        Node rootNodeNeighbour31 = new Node(8);
+        rootNodeNeighbour31.neighbours.add(rootNodeNeighbour21);
+        rootNodeNeighbour21.neighbours.add(rootNodeNeighbour31);
+
+        Node rootNodeNeighbour32 = new Node(9);
+        rootNodeNeighbour32.neighbours.add(rootNodeNeighbour22);
+        rootNodeNeighbour22.neighbours.add(rootNodeNeighbour32);
+
+        Node rootNodeNeighbour33 = new Node(10);
+        rootNodeNeighbour33.neighbours.add(rootNodeNeighbour22);
+        rootNodeNeighbour22.neighbours.add(rootNodeNeighbour33);
 
         return graph;
     }
